5a603a3df9e7092f0ef48b3f0e0f769c4509b396
narasimha110 <narasimha4154@gmail.com>
2025-06-12 20:03:39 +0530

    siva

--- src/Nonveg.jsx
+++ src/Nonveg.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { addToCart } from './store'; // Adjust path as needed
-import './Veg.css'; // Reuse the same styling
+import './veg.css'; // Reuse the same styling
 
 function NonVeg() {
   const dispatch = useDispatch();
